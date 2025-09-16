fccaad5cda0813eae4ab01bddb2d08b8a75d8beb
Gavin Payne <gavinapayne@gmail.com>
2025-09-16 17:57:51 +0400

    v1.0.11

--- src/DynamoDBFetch/node/index.ts
+++ src/DynamoDBFetch/node/index.ts
@@ -7,7 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
   
   return {
-    packageVersion: "1.0.9",
+    packageVersion: "1.0.10",
     type: NODE_TYPE,
     name: "DynamoDB Fetch",
     description: "Fetch a record from AWS DynamoDB table by key",
--- src/DynamoDBService/node/index.ts
+++ src/DynamoDBService/node/index.ts
@@ -7,7 +7,7 @@ function createNodeDefinition(): EnhancedNodeDefinition {
   const { NodeInputType } = getPlatformDependencies();
   
   return {
-    packageVersion: "1.0.9",
+    packageVersion: "1.0.10",
     type: NODE_TYPE,
     name: "DynamoDB Service",
     description: "Provides DynamoDB operations as a service for other nodes",
